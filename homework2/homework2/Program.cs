67b5eb3a4f1a0d4b42f621edd397263f766f8218
Евгений <jack-carver@bk.ru>
2016-01-23 00:23:05 +0200

    com1

---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1 t1 = new Task1();
+            t1.DoSomething();
+
+            Task2 t2 = new Task2();
+            t2.DoSomething();
+
+            Task3 t3 = new Task3();
+            t1.DoSomething();
+
+            Task4 t4 = new Task4();
+            t1.DoSomething();
+        }
+    }
+}
---
+++ homework2/homework2/Task1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+    class Task1
+    {
+        public void DoSomething()
+        {
+            Console.ReadKey();
+        }
+    }
+}
+
