c8add6473977bbb998a85446e875cc0c45e165ed
Евгений <jack-carver@bk.ru>
2016-01-28 14:08:51 +0200

    t2

--- homework3/homework3/Task2.cs
+++ homework3/homework3/Task2.cs
@@ -10,7 +10,58 @@ namespace homework3
     {
         public void DoSomething()
         {
+            Player player = new Player();
+            player.Play();
+            (player as IPlayable).Pause();
+            (player as IPlayable).Stop();
+
+            player.Record();
+            (player as IRecordable).Pause();
+            (player as IRecordable).Stop();
             Console.ReadKey();
+        }
+
+        public interface IPlayable
+        {
+            void Play();
+            void Pause();
+            void Stop();
+        }
+
+        public interface IRecordable
+        {
+            void Record();
+            void Pause();
+            void Stop();
+        }
+
+        public class Player : IPlayable , IRecordable
+        {
+            public void Play()
+            {
+                Console.WriteLine("Play");
+            }
+            void IPlayable.Pause()
+            {
+                Console.WriteLine("Pause play");
+            }
+            void IPlayable.Stop()
+            {
+                Console.WriteLine("Stop play");
+            }
+
+            public void Record()
+            {
+                Console.WriteLine("Record");
+            }
+            void IRecordable.Pause()
+            {
+                Console.WriteLine("Pause record");
+            }
+            void IRecordable.Stop()
+            {
+                Console.WriteLine("Stop record");
+            }
         }
     }
 }
