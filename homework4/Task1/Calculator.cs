6cc4072b917df079d3f3bfdbc8b0b7085dedf191
Евгений <jack-carver@bk.ru>
2016-01-30 23:48:24 +0200

    t1

---
+++ homework4/Task1/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class Calculator
+    {
+        public static void Sum<S1 , S2>(S1 sum1 , S2 sum2)
+        {
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+        }
+    }
+}
---
+++ homework4/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 45;
+            double b = 45.45;
+            Calculator.Sum(a,b);
+
+            Console.ReadKey();
+        }
+    }
+}
