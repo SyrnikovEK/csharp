40a1127948677cded1e958459a6dd3c747e8cbba
Евгений <jack-carver@bk.ru>
2016-02-01 15:06:44 +0200

    t2

---
+++ homework4/Task2/ArrayExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class ArrayExtensions
+    {
+        public static void MySort(this int[] arr , int left , int right) 
+        {
+            int l = left;
+            int r = right;
+            int tmp;
+            int mid = arr[(r+l) / 2];
+
+            while (l <= r)
+            {
+                while (arr[l] < mid)
+                {
+                    l++;
+                }
+                while (arr[r] > mid)
+                {
+                    r--;
+                }
+                if (l <= r)
+                {
+                    tmp = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = tmp;
+
+                    l++;
+                    r--;
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            if (l < right)
+            {
+                MySort(arr, l, right);
+            }
+            if (r > left)
+            {
+                MySort(arr, left, r);
+            }
+          
+        }
+    }
+}
---
+++ homework4/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r1 = new Random();
+            //int[] arr = new int[]{ 2 , 4 , 1 , 3 , 5 , 9 , 6 , 0 , 8 , 7};
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write(arr[i] + " ");
+            //}
+
+            //arr.MySort(0,arr.Length-1);
+
+
+            int[] arr2 = new int[10];
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                arr2[i] = r1.Next(0,100);
+            }
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+            arr2.MySort(0, arr2.Length - 1);
+
+            Console.ReadKey();
+        }
+    }
+}
