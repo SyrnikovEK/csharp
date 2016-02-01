2b29ad7752943e4cc555d051fe9194fbb6652513
Евгений <jack-carver@bk.ru>
2016-02-01 23:36:24 +0200

    t2 not all

---
+++ homework5/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int number;
+            DateTime time;
+            Train[] trainarray = new Train[3];
+            Console.WriteLine("Input info about 8 trains");
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.WriteLine("Input Name");
+                name = Console.ReadLine();
+                Console.WriteLine("Input Number");
+                number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Input Time");
+                time = DateTime.Parse(Console.ReadLine());
+                //trainarray[i] = new Train(name , number , time);
+            }
+
+            for (int i = 0; i <= 3; i++)
+            {
+                //trainarray[i].ToString();
+            }
+        }
+    }
+}
---
+++ homework5/Task2/Train.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    struct Train
+    {
+        string Name;
+        int Number;
+        DateTime Time;
+
+        public Train(string name , int number , DateTime time)
+        {
+            Name = name;
+            Number = number;
+            Time = time;
+        }
+    }
+}
