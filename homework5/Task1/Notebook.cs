ef555c64d044f11e4421c4913c1633beb3e6058c
Евгений <jack-carver@bk.ru>
2016-02-01 22:49:00 +0200

    t1

---
+++ homework5/Task1/Notebook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Notebook
+    {
+        string Name;
+        string Manufacturer;
+        double Price;
+        public Notebook(string name , string manufacturer , int price)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Name: {0}, Manufacturer: {1}, Price: {2}" , Name , Manufacturer , Price);
+        }
+    }
+}
---
+++ homework5/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook1 = new Notebook("Qwe", "Asd", 100);
+            notebook1.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
