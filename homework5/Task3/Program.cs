a8b00cce3c3ce76f448b6d393526ec7113f2c53e
Евгений <jack-carver@bk.ru>
2016-02-02 15:23:51 +0200

    t3

---
+++ homework5/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass1 = new MyClass();
+            MyStruct mySrtuct1 = new MyStruct();
+
+            myClass1.change = "didn't change";
+            mySrtuct1.change = "didn't change";
+            ClassTaker(myClass1);
+            StructTaker(mySrtuct1);
+            Console.WriteLine("Class " + myClass1.change);
+            Console.WriteLine("Struct " + mySrtuct1.change);
+
+            Console.ReadKey();
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "changed";
+        }
+
+        static void StructTaker(MyStruct myStruct)
+        {
+            myStruct.change = "changed";
+        }
+    }
+}
