ebe364e41a67895fee6fc1bb07ec7a89f2515773
Евгений <jack-carver@bk.ru>
2016-02-02 16:08:16 +0200

    t5

---
+++ homework5/Task5/ColorfullInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public static class ColorfullInput
+    {
+        public static void Print(string inputstring , int color)
+        {
+            switch (color)
+            {
+                case 1:
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        break;
+                    }
+                default:
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        break;
+                    }
+            }
+
+            Console.WriteLine(inputstring);
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ homework5/Task5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            string color;
+            Console.WriteLine("Input string");
+            str = Console.ReadLine();
+            Console.WriteLine("Input color");
+            color = Console.ReadLine();
+            color.ToLower();
+
+            switch (color)
+            {
+                case "red":
+                    {
+                        ColorfullInput.Print(str, (int)Color.Red);
+                        break;
+                    }
+                case "green":
+                    {
+                        ColorfullInput.Print(str, (int)Color.Green);
+                        break;
+                    }
+                case "blue":
+                    {
+                        ColorfullInput.Print(str, (int)Color.Blue);
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
