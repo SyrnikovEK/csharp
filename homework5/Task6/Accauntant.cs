1f1f677ba5a4f6ff656494e9af7f96d8f0f84c1f
Евгений <jack-carver@bk.ru>
2016-02-02 16:38:35 +0200

    t6

---
+++ homework5/Task6/Accauntant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public static class Accauntant
+    {
+        public static bool AskForBonus(Position worker , int hours)
+        {
+            if (hours > (int)worker)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
