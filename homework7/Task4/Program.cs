d6c6037bce4b8901edb548cf69927cbd580e02ba
Евгений <jack-carver@bk.ru>
2016-02-08 14:19:20 +0200

    t4

--- homework7/Task4/Program.cs
+++ homework7/Task4/Program.cs
@@ -10,6 +10,19 @@ namespace Task4
     {
         static void Main(string[] args)
         {
+            MyList<int> myList = new MyList<int>(10);
+            int[] arr = new int[0];
+            myList[3] = 2;
+            myList.Add(3);
+
+            arr = myList.GetArray();
+
+            for (int i = 0; i < myList.Length; i++)
+            {
+                Console.WriteLine("List: {0}    Array: {1}" , myList[i] , arr[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
