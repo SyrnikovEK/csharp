0bd8bee2ef6c7fb26d181733fac7ee819dd0c1bb
Евгений <jack-carver@bk.ru>
2016-02-08 13:44:21 +0200

    t2

---
+++ homework7/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>(10);
+            myList[3] = 2;
+            myList.Add(3);
+
+            for (int i = 0; i < myList.Length; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
