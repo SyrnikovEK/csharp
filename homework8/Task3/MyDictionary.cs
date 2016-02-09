4bcb69557dffc970696cbdfa2a4c423e723da80f
Евгений <jack-carver@bk.ru>
2016-02-09 15:29:46 +0200

    t3

---
+++ homework8/Task3/MyDictionary.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Task3
+{
+    public class MyDictionary<TKey, TValue> : IEnumerable
+    {
+        public class KeyValuePair//<TKey , TValue>
+        {
+            public TKey key;
+            public TValue value;
+            public KeyValuePair(TKey skey, TValue svalue)
+            {
+                key = skey;
+                value = svalue;
+            }
+            public KeyValuePair()
+            {
+                key = default(TKey);
+                value = default(TValue);
+            }
+
+            //public string ToString()
+            //{
+            //    return key.ToString() + " " + value.ToString();
+            //}
+        }
+
+        private int length;
+        private KeyValuePair[] items;
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+        public MyDictionary()
+        {
+            length = 0;
+            items = new KeyValuePair[0];
+        }
+
+        public MyDictionary(int count)
+        {
+            length = count;
+            items = new KeyValuePair[count];
+        }
+
+
+        public KeyValuePair this[TKey index]
+        {          
+            get
+            {
+                KeyValuePair returnedItem = new KeyValuePair();
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i].key.Equals(index)) //
+                    {
+                        //return items[i];
+                        returnedItem = items[i];
+                        break;
+                    }
+                }
+                return returnedItem;
+            }
+
+            set
+            {
+                KeyValuePair returnedItem = new KeyValuePair();
+                int i;
+                for (i = 0; i < items.Length; i++)
+                {
+                    if (items[i].key.Equals(index)) //
+                    {
+                        //return items[i];
+                        returnedItem = items[i];
+                        break;
+                    }
+                }
+
+                if (returnedItem != null)
+                {
+                    items[i] = value;
+                }
+            }
+        }
+
+        public void Add(TKey key , TValue value)//(KeyValuePair item)
+        {
+            KeyValuePair[] newitems = new KeyValuePair[length];
+            Array.Copy(items, newitems, length);
+            items = new KeyValuePair[length + 1];
+            Array.Copy(newitems, items, newitems.Length);
+            items[length] = new KeyValuePair(key , value);
+            length++;
+        }
+
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return items.GetEnumerator();  // ?
+        }
+    }
+}
---
+++ homework8/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+
+            myDictionary.Add(1, "one");
+            myDictionary.Add(2, "two");
+            myDictionary.Add(3, "three");
+            myDictionary.Add(4, "four");
+            myDictionary.Add(5, "five");
+
+            foreach (var item in myDictionary)
+            {
+                Console.WriteLine(item);  // ToString not working?
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
