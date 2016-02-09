c506be65bb300447badff40cffca59a41a758a7b
Евгений <jack-carver@bk.ru>
2016-02-09 14:56:41 +0200

    t1

---
+++ homework8/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public delegate int[] MyDelegate(int[] arr);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = (int[] arr) =>
+            {
+                int[] returnedArr = new int[(arr.Length / 2) + 1];
+                int j = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    returnedArr[j] = arr[i];
+                    i++;
+                    j++;
+                }
+                return returnedArr;
+            };
+
+            int[] oldArr = new int[5] {1 , 2 , 3 , 4 , 5 };
+            int[] newArr = new int[0];
+            newArr = myDelegate(oldArr);
+
+            for (int i = 0; i < oldArr.Length; i++)
+            {
+                Console.Write(oldArr[i] + " ");
+                if (i < newArr.Length)
+                {
+                    Console.Write(" " + newArr[i]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
