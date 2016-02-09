07893f77df61296c322d62fbbfe84274a4ea3e0b
Евгений <jack-carver@bk.ru>
2016-02-09 15:06:29 +0200

    t2

---
+++ homework8/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list1 = new MyList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list1.Add(i);
+            }
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
