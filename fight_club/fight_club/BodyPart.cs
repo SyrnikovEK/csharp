8c7ba0c82a45277b77c188986f1db295fa0877b7
Евгений <jack-carver@bk.ru>
2016-02-11 14:48:02 +0200

    made first ui

---
+++ fight_club/fight_club/BodyPart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fight_club
+{
+    public enum BodyPart
+    {
+        Head , 
+        Body ,
+        Feet ,
+        LeftHand , 
+        RightHand
+    }
+}
