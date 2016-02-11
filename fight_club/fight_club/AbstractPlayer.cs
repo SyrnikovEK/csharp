5423b6bf2a214f0b2dec16fb6371126f830a423f
Евгений <jack-carver@bk.ru>
2016-02-11 22:15:15 +0200

    add some classes

---
+++ fight_club/fight_club/AbstractPlayer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fight_club
+{
+    public abstract class AbstractPlayer : IPlayer
+    {
+        private int level;
+        private int hp;
+        private string name;
+        private int straight;
+        private int agility;
+        private int stamina;
+        private BodyPart blockedPart;
+
+        public int Level
+        {
+            get
+            {
+                return level;
+            }
+        }
+        public int Hp
+        {
+            get
+            {
+                return hp;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public int Straight
+        {
+            get
+            {
+                return straight;
+            }
+        }
+        public int Agility
+        {
+            get
+            {
+                return straight;
+            }
+        }
+        public int Stamina
+        {
+            get
+            {
+                return straight;
+            }
+        }
+
+        public abstract void SetBlock(BodyPart part);
+        public void GetHit(BodyPart part , FightPapams par)
+        {
+            if (par.agility > agility)
+            {
+                hp -= par.straight * 5;
+            }
+            else
+            {
+                if (Dice.Throw() > 14)
+                {
+                    hp -= par.straight * 5;
+                }         
+            }
+        }
+    }
+}
---
+++ fight_club/fight_club/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fight_club
+{
+    public interface IPlayer
+    {
+        void SetBlock(BodyPart part);
+        void GetHit(BodyPart part , FightPapams par);
+    }
+}
