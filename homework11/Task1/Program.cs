6faf61f78891809a0110c3694c43877879cd1015
Евгений <jack-carver@bk.ru>
2016-02-16 16:18:40 +0200

    t1

---
+++ homework11/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory;
+            string newdirpath = "";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            Console.WriteLine(dirInfo.FullName);
+
+            for (int i = 0; i < 2; i++)
+            {
+                newdirpath = path + @"\mydir" + i;
+                dirInfo = new DirectoryInfo(newdirpath);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }               
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                newdirpath = path + @"\mydir" + i;
+                dirInfo = new DirectoryInfo(newdirpath);
+                if (dirInfo.Exists)
+                {
+                    dirInfo.Delete();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
