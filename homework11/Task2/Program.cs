d973b4d3d6b209c851fab8884cc7cca5c72f5d18
Евгений <jack-carver@bk.ru>
2016-02-16 16:36:06 +0200

    t2

---
+++ homework11/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 101;
+            string b = "bbb";
+            bool c = true;
+            double d = 5.05;
+
+            Console.WriteLine("int: {0} string: {1} bool: {2} double: {3}" , a , b , c , d);
+
+            var stream = new FileStream("file.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            var writer = new BinaryWriter(stream);
+            
+            writer.Write(a);
+            writer.Write(b);
+            writer.Write(c);
+            writer.Write(d);
+
+            writer.Close();
+            stream.Close();
+
+            stream = new FileStream("file.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            var reader = new BinaryReader(stream);
+
+            Console.WriteLine("int: {0} string: {1} bool: {2} double: {3}", reader.ReadInt32() , reader.ReadString() , reader.ReadBoolean() , reader.ReadDouble());
+            
+            reader.Close();
+            stream.Close();
+            
+
+            Console.ReadKey();
+        }
+    }
+}
