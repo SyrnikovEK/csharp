9c42a49e015dcbdb3032383f43f4b52d488435ee
Евгений <jack-carver@bk.ru>
2016-02-18 15:19:54 +0200

    add playersFB (almost clean)

---
+++ fight_club/fight_club/PlayersDB.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fight_club
+{
+    public class PlayersDB
+    {
+        private AbstractPlayer[] elements;
+        private int count;
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public PlayersDB()
+        {
+            elements = new AbstractPlayer[0];
+            count = 0;
+        }
+
+        public bool IsExist(string name)
+        {
+            bool exist = false;
+            for (int i = 0; i < count; i++)
+            {
+                if (elements[i].Name == name)
+                {
+                    exist = true;
+                }
+            }
+            return exist;
+        }
+
+        public void Add(AbstractPlayer player)
+        {
+
+        }
+
+        public void Remove(string name)
+        {
+
+        }
+
+        public AbstractPlayer Get(string name)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (elements[i].Name == name)
+                {
+                    return elements[i];
+                }
+            }
+            return null;
+        }
+    }
+}
