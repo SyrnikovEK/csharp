4c98003a8eea7239783c758b19e5de520349c79f
Евгений <jack-carver@bk.ru>
2016-03-14 14:24:07 +0200

    Cover all code

---
+++ FirTreeProject/FirTreeProject/FirTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirTreeProject
+{
+    public class FirTree : ITree
+    {
+        private double height;
+        private double growingSpeed;
+        private TreeColor color;
+        private TreeType type;
+        private TreeShape shape;
+
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+        public double GrowingSpeed
+        {
+            get
+            {
+                return growingSpeed;
+            }
+            set
+            {
+                growingSpeed = value;
+            }
+        }
+        public TreeColor Color
+        {
+            get
+            {
+                return color;
+            }
+        }
+        public TreeType Type
+        {
+            get
+            {
+                return type;
+            }
+        }
+        public TreeShape Shape
+        {
+            get
+            {
+                return shape;
+            }
+        }
+
+        public FirTree()
+        {
+            height = 0;
+            color = TreeColor.Green;
+            type = TreeType.FirTree;
+            shape = TreeShape.Shapely;
+        }
+
+        public void GrowUp(double delta)
+        {
+            height += delta;
+        }
+
+        public void GrowUp()
+        {
+            height += growingSpeed;
+        }
+    }
+}
