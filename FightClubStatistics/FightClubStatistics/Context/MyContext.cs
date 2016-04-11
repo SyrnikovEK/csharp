4c4bcc38cb24f614e6b77d598c092b653f85302e
Евгений <jack-carver@bk.ru>
2016-04-11 15:42:02 +0300

    add initialization

--- FightClubStatistics/FightClubStatistics/Context/MyContext.cs
+++ FightClubStatistics/FightClubStatistics/Context/MyContext.cs
@@ -9,6 +9,11 @@ namespace FightClubStatistics
 {
    public class MyContext : DbContext , IContext
     {
+        public MyContext()
+        {
+            Database.SetInitializer<MyContext>(new MyContextInitializer());
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<CombatLog> CombatLogs { get; set; }
